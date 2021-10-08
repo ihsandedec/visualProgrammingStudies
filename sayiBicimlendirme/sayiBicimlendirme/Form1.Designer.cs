e6ab98d99296fbed8d8959dbfb9a37e5ec1e0e73
ihsandedec <ddcihsan@gmail.com>
2021-10-08 11:32:23 +0300

    Sayıları biçimlendirme örneği eklendi

---
+++ sayiBicimlendirme/sayiBicimlendirme/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace sayiBicimlendirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(23, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "C and C_number";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(167, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "N and N_number";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(309, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "E and E_number";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(313, 146);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "X";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(171, 146);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 33);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "P";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(27, 146);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 33);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "F";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(167, 222);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(112, 33);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "X and X_number";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 324);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ sayiBicimlendirme/sayiBicimlendirme/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayiBicimlendirme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Bazı durumlarda sayının ekranda gösterilmesi esnasında değişik bir formatta gösterilmesi gerekebilir.
+        // Bunu gerçekleştirebilmek için değişken_adı.ToString(“Format_Harfi”) şeklinde yazım yapılır.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi;
+            sayi = 15000000;
+            MessageBox.Show(sayi.ToString("C"));
+            MessageBox.Show(sayi.ToString("C0"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double sayi;
+            sayi = 15000000;
+            MessageBox.Show(sayi.ToString("N"));
+            MessageBox.Show(sayi.ToString("N0"));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double sayi;
+            sayi = 15000000;
+            MessageBox.Show(sayi.ToString("E"));
+            MessageBox.Show(sayi.ToString("E4"));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double sayi;
+            sayi = 23.451784738;
+            MessageBox.Show(sayi.ToString("F2"));
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double sayi;
+            sayi = 0.23;
+            MessageBox.Show(sayi.ToString("P"));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sayi;
+            sayi = 236;
+            MessageBox.Show(sayi.ToString("X"));
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int sayi;
+            sayi = 236;
+            MessageBox.Show(sayi.ToString("X"));
+            MessageBox.Show(sayi.ToString("X4"));
+        }
+    }
+}
