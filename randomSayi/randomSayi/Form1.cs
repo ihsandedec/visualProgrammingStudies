6fafa1f5068b5c1fb515f91808b5d1eec591f4de
ihsandedec <ddcihsan@gmail.com>
2021-10-10 16:26:09 +0300

    Random sayı örneği eklendi

---
+++ randomSayi/randomSayi/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace randomSayi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rastgele = new Random();
+            int üretilen_sayi;
+            üretilen_sayi = rastgele.Next();
+            textBox1.Text = üretilen_sayi.ToString();
+        }
+    }
+}
