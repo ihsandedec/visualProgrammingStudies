b79f8b50700b86c5d387e8a59eaacbabc92b4dfc
ihsandedec <ddcihsan@gmail.com>
2021-10-12 21:21:29 +0300

    String arama işlem örneği eklendi

---
+++ stringSearch/stringSearch/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stringSearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ifade;
+            ifade = "Visual c# ile nesneye yönelik programlama";
+            MessageBox.Show(ifade.IndexOf("yöne",0).ToString());
+        }
+    }
+}
