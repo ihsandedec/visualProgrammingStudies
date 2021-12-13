26cedc65af5817cede8e7632ab56669944eabd38
ihsandedec <ddcihsan@gmail.com>
2021-12-13 21:11:16 +0300

    messageBox05 örneği eklendi

---
+++ messageBox.Show(M,B,D,S,V_D)/sayfa66_MessageBox.Show(M, B, D, S, V_D)/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa66_MessageBox.Show_M__B__D__S__V_D_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBoxDefaultButton ile seçili button üzerinde vurgulama yapılır.
+            MessageBox.Show("Çıkamak İstediğinizden Emin Misiniz?", "ONAY", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dosyayı Silmek İstiyor Musunuz?", "ONAY", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
+        }
+    }
+}
