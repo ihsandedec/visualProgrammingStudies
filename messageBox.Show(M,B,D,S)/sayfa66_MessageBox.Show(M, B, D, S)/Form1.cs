1d8487082cb4e86b5170cd6d9000f775237657e2
ihsandedec <ddcihsan@gmail.com>
2021-12-13 21:10:19 +0300

    messageBox04 örneği eklendi

---
+++ messageBox.Show(M,B,D,S)/sayfa66_MessageBox.Show(M, B, D, S)/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa66_MessageBox.Show_M__B__D__S_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çıkamak İstediğinizden Emin Misiniz?", "ONAY", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Çıkamak İstediğinizden Emin Misiniz?", "ONAY", MessageBoxButtons.AbortRetryIgnore,MessageBoxIcon.Warning);
+        }
+    }
+}
