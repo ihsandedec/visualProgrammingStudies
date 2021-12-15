7d37d5a7f6be8d12223da46930562bad7da09307
ihsandedec <ddcihsan@gmail.com>
2021-12-16 00:05:44 +0300

    Menü Strip ile ilgili örnek eklendi

---
+++ menuStrip/sayfa104_MenuStrip/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa104_MenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int say, y = 30;
+        private void ekleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Button komut_dugmesi = new Button();
+            komut_dugmesi.Top = y;
+            komut_dugmesi.Left = 0;
+            komut_dugmesi.Width = 150;
+            say++;
+            komut_dugmesi.Text = "Yeni Komut Düğmesi " + say.ToString();
+            y += komut_dugmesi.Height + 5;
+
+            komut_dugmesi.Click += new EventHandler(tıkla);
+
+            this.Controls.Add(komut_dugmesi);
+        }
+
+        private void silToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(this.ActiveControl);
+        }
+
+        private void tıkla(object sender, System.EventArgs e)
+        {
+            MessageBox.Show((sender as Button).Text + " tıklandı");
+        }
+
+    }
+}
