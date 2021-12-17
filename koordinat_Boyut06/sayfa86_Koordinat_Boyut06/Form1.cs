93073d0a1909017845a9e2c64f7035a8731b355f
ihsandedec <ddcihsan@gmail.com>
2021-12-17 07:44:15 +0300

    Koordinat06 örneği eklendi

---
+++ koordinat_Boyut06/sayfa86_Koordinat_Boyut06/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa86_Koordinat_Boyut06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ifadeler = { "Yok", "Sağa", "Sola", "Üste", "Alta", "İçine" };
+            comboBox1.Items.AddRange(ifadeler);
+            comboBox2.Items.AddRange(ifadeler);
+            comboBox3.Items.AddRange(ifadeler);
+        }
+
+        private void dock_ayarlari(object sender, EventArgs e)
+        {
+            if ((sender as ComboBox).Text == "Yok")
+            {
+                (sender as ComboBox).Parent.Dock = DockStyle.None;
+            }
+            else if ((sender as ComboBox).Text == "Sağa")
+            {
+                (sender as ComboBox).Parent.Dock = DockStyle.Right;
+            }
+            else if ((sender as ComboBox).Text == "Sola")
+            {
+                (sender as ComboBox).Parent.Dock = DockStyle.Left;
+            }
+            else if ((sender as ComboBox).Text == "Üste")
+            {
+                (sender as ComboBox).Parent.Dock = DockStyle.Top;
+            }
+            else if ((sender as ComboBox).Text == "Alta")
+            {
+                (sender as ComboBox).Parent.Dock = DockStyle.Bottom;
+            }
+            else if ((sender as ComboBox).Text == "İçine")
+            {
+                (sender as ComboBox).Parent.Dock = DockStyle.Fill;
+            }
+        }
+
+        
+    }
+}
