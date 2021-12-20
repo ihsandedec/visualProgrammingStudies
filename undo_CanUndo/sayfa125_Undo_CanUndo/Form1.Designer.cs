bdd21c95f95413f9cdae0a76664de4adeed3f7ae
ihsandedec <ddcihsan@gmail.com>
2021-12-20 17:19:39 +0300

    Undo ve CanUndo örneği eklendi

---
+++ undo_CanUndo/sayfa125_Undo_CanUndo/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace sayfa125_Undo_CanUndo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_gerial = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btn_gerial
+            // 
+            this.btn_gerial.Location = new System.Drawing.Point(217, 69);
+            this.btn_gerial.Name = "btn_gerial";
+            this.btn_gerial.Size = new System.Drawing.Size(75, 23);
+            this.btn_gerial.TabIndex = 1;
+            this.btn_gerial.Text = "Geri Al";
+            this.btn_gerial.UseVisualStyleBackColor = true;
+            this.btn_gerial.Click += new System.EventHandler(this.btn_gerial_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 181);
+            this.Controls.Add(this.btn_gerial);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_gerial;
+    }
+}
+
---
+++ undo_CanUndo/sayfa125_Undo_CanUndo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa125_Undo_CanUndo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_gerial_Click(object sender, EventArgs e)
+        {
+            if (textBox1.CanUndo)
+            {
+                textBox1.Undo();
+            }
+            else
+            {
+                MessageBox.Show("Geri alma işleminiz gerçekleştirilemez");
+            }
+        }
+    }
+}
