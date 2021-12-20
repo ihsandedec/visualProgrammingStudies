22b2f6a682c00a951cf46e13fde691fb0948c79e
ihsandedec <ddcihsan@gmail.com>
2021-12-20 17:19:16 +0300

    TexBoxda modifiers örneği eklendi

---
+++ textBox1_Modifiers/sayfa111_textBox1_Modifiers/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa111_textBox1_Modifiers
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public int x;
+      
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "x değişkeninin değeri= " + x.ToString();
+        }
+    }
+}
