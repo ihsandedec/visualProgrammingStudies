22069bed55125276f46aefbc520e144859a4c7eb
ihsandedec <ddcihsan@gmail.com>
2021-12-25 14:41:05 +0300

    Closing gizleme örneği eklendi

---
+++ closing_kapat_gizle/sayfa107_Closing_kapat_gizle/Form2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa107_Closing_kapat_gizle
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+    }
+}
