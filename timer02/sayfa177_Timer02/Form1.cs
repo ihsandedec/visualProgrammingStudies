6264ef67302a18c397cad8a5ce3476e449422c49
ihsandedec <ddcihsan@gmail.com>
2021-12-26 15:01:05 +0300

    Timer ikinci örnek eklendi

---
+++ timer02/sayfa177_Timer02/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa177_Timer02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 100;
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Color c;
+            c = textBox1.BackColor;
+            textBox1.BackColor = textBox1.ForeColor;
+            textBox1.ForeColor = c;
+        }
+    }
+}
