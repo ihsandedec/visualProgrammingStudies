2c5d38aa8ddc40bd0fd75e0789f518ba793c35bc
ihsandedec <ddcihsan@gmail.com>
2021-12-27 10:31:08 +0300

    Tip dönüşümleri ikinci örneği eklendi

---
+++ tipDonusumleri2/sayfa18_TipDonusumleri2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa18_TipDonusumleri2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        long islem(long x, long y)
+        {
+            return x * y;
+        }
+
+        int islem(int x, int y)
+        {
+            return x + y;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long sonuc;
+            int x = 2, y = 5;
+            sonuc = islem(x,y);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            long sonuc;
+            int x = 2, y = 5;
+            sonuc = islem((long)x, (long)y);
+            
+        }
+
+    }
+}
