d2567dd97c5e8342d47aa38ca79c5b0fc381c32b
ihsandedec <ddcihsan@gmail.com>
2021-12-27 10:31:49 +0300

    Global ve Local tanımlama örneği eklendi

---
+++ globalAndLocal/sayfa19_GlobalAndLocal/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa19_GlobalAndLocal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Local Tanımlama
+            int a;
+            a = 10;
+        }
+
+        int a;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Global Tanımlama
+            a = 10;
+        }
+    }
+}
