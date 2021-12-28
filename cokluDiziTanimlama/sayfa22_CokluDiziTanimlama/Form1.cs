cffabf30928d06c115da34d9fdc63dde122fd4ed
ihsandedec <ddcihsan@gmail.com>
2021-12-28 13:47:38 +0300

    Çoklu dizi tanımlama örneği eklendi

---
+++ cokluDiziTanimlama/sayfa22_CokluDiziTanimlama/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa22_CokluDiziTanimlama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] dizi = new int[2, 3];
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    dizi[i, j] = i * 10 + j;
+                }
+            }
+        }
+    }
+}
