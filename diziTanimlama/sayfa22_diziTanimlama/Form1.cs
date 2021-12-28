20e876d3d25dea1ec505528ef029a0c494ed12cc
ihsandedec <ddcihsan@gmail.com>
2021-12-28 13:47:13 +0300

    Dizi tanımlama örneği eklendi

---
+++ diziTanimlama/sayfa22_diziTanimlama/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa22_diziTanimlama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] dizi = new int[15];
+            for (int i = 0; i < 15; i++)
+            {
+                dizi[i] = i * 10;
+            }
+           
+        }
+    }
+}
