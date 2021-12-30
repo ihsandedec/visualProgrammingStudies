e6aec610194d4b313a0b686e9d254b8610a96ea0
ihsandedec <ddcihsan@gmail.com>
2021-12-30 16:30:11 +0300

    FindString Exat örneği eklendi

---
+++ findStringExat/sayfa154_FindStringExat/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa154_FindStringExat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] isimler = { "Ahmet", "Serdar", "İhsan", "Uğur", "Erkan", "Ayşe", "Fatma" };
+            listBox1.Items.AddRange(isimler);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int yer;
+            string aranan;
+            aranan = textBox1.Text;
+            yer = listBox1.FindStringExact(aranan);
+
+            if (yer < 0)
+            {
+                MessageBox.Show("Aranan İfade Bulunamadı");
+            }
+            else
+            {
+                MessageBox.Show((yer+1).ToString() + " . eleman");
+                listBox1.SelectedIndex = yer;
+            }
+        }
+    }
+}
