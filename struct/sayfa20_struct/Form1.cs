c4fcafa6d53c2ddb30bd066eddabdbccda5e66bc
ihsandedec <ddcihsan@gmail.com>
2022-01-02 22:52:00 +0300

    Struct örnği eklendi

---
+++ struct/sayfa20_struct/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa20_struct
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        struct personel
+        {
+            public string adi;
+            public string soyadi;
+            public string ev_adresi;
+            public string ev_telefonu;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            personel personel_kayit = new personel();
+            personel_kayit.adi = "İhsan";
+            personel_kayit.soyadi = "Dedeç";
+            personel_kayit.ev_adresi = "Denizli";
+            personel_kayit.ev_telefonu = "xxxxxxxx";
+            MessageBox.Show(personel_kayit.adi.ToString()+ " "+personel_kayit.soyadi.ToString()+ " " + personel_kayit.ev_adresi.ToString()+ " " + personel_kayit.ev_telefonu.ToString());
+        }
+    }
+}
---
+++ struct/sayfa20_struct/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("sayfa20_struct")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sayfa20_struct")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("9bdae228-5c71-4f6e-92d6-331e5e54e84e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
