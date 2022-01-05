bae832b673a778ece5a3fae1fe6a04d7075edcce
ihsandedec <ddcihsan@gmail.com>
2022-01-05 17:32:10 +0300

    Sayılar arasında büyük sayıyı bulma örneği eklendi

---
+++ buyukBulma/sayfa48_buyukBulma/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa48_buyukBulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int max(int sayi1, int sayi2)
+        {
+            int donendeger = 0; ;
+            if (sayi1 > sayi2)
+            {
+                donendeger = sayi1;
+            }
+            else if (sayi2 > sayi1)
+            {
+                donendeger = sayi2;
+            }
+            return donendeger;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gelensayi = max(40, 20);
+            MessageBox.Show(gelensayi.ToString());
+        }
+    }
+}
