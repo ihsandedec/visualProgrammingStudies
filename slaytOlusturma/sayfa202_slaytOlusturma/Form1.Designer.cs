516769031064e381fa7526eb413c755d4f6d8568
ihsandedec <ddcihsan@gmail.com>
2022-01-09 11:19:20 +0300

    Slayt oluşturma örneği eklendi

---
+++ slaytOlusturma/sayfa202_slaytOlusturma/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace sayfa202_slaytOlusturma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_resimgoster = new System.Windows.Forms.Button();
+            this.button_dosyasil = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_resimgoster
+            // 
+            this.button_resimgoster.Location = new System.Drawing.Point(33, 41);
+            this.button_resimgoster.Name = "button_resimgoster";
+            this.button_resimgoster.Size = new System.Drawing.Size(114, 29);
+            this.button_resimgoster.TabIndex = 0;
+            this.button_resimgoster.Text = "Resim Gösterisi";
+            this.button_resimgoster.UseVisualStyleBackColor = true;
+            this.button_resimgoster.Click += new System.EventHandler(this.button_resimgoster_Click);
+            // 
+            // button_dosyasil
+            // 
+            this.button_dosyasil.Location = new System.Drawing.Point(203, 41);
+            this.button_dosyasil.Name = "button_dosyasil";
+            this.button_dosyasil.Size = new System.Drawing.Size(114, 29);
+            this.button_dosyasil.TabIndex = 1;
+            this.button_dosyasil.Text = "Dosya Sil";
+            this.button_dosyasil.UseVisualStyleBackColor = true;
+            this.button_dosyasil.Click += new System.EventHandler(this.button_dosyasil_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(46, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 268);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.HelpRequest += new System.EventHandler(this.openFileDialog1_HelpRequest);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.HelpRequest += new System.EventHandler(this.openFileDialog2_HelpRequest);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 436);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_dosyasil);
+            this.Controls.Add(this.button_resimgoster);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_resimgoster;
+        private System.Windows.Forms.Button button_dosyasil;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ slaytOlusturma/sayfa202_slaytOlusturma/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa202_slaytOlusturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer1.Interval = 1000;
+
+            openFileDialog1.ShowHelp = true;
+            openFileDialog1.Filter = "Resim dosyaları | .bmp;.jpeg;.gif;.tif";
+            openFileDialog1.Title = "Gösterilecek Resimleri Seçiniz";
+            openFileDialog1.Multiselect = true;
+
+            openFileDialog2.ShowHelp = true;
+            openFileDialog2.Filter = "Geçici Dosyalar | *.tmp;*.~* |" + "Bütün Dosyalar | *.*";
+            openFileDialog2.Title = "Silinecek Dosyaları Seçiniz";
+            openFileDialog2.Multiselect = true;
+
+        }
+
+        private void button_resimgoster_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                timer1.Enabled = true;
+            }
+        }
+
+        private void button_dosyasil_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog()==DialogResult.OK)
+            {
+                DialogResult c;
+                c = MessageBox.Show("Seçtiğinz bütün dosyalar silinecektir.","Silme İşlemi",MessageBoxButtons.OKCancel,MessageBoxIcon.Warning,MessageBoxDefaultButton.Button2);
+                if (c==DialogResult.OK)
+                {
+                    for (int i = 0; i < openFileDialog2.FileNames.Length; i++)
+                    {
+                        try
+                        {
+                            System.IO.FileInfo dosya = new System.IO.FileInfo(openFileDialog2.FileNames[i]);
+                            dosya.Delete();
+                        }
+                        catch 
+                        {
+
+                            MessageBox.Show(openFileDialog2.FileNames[i]+"Dosya Silinemedi");
+                        }
+                    }
+                }
+            }
+        }
+        int say = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            say++;
+            say = say % openFileDialog1.FileNames.Length;
+            try
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileNames[say]);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show(openFileDialog1.FileNames[say]+" dosyası geçersizdir");
+            }
+        }
+
+        private void openFileDialog1_HelpRequest(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gösterilecek resimleri seçiniz. Birden fazla resmi seçebilmek" + " için Ctrl tuşuna basılı tutarak istediğiniz resmi seçiniz.");
+
+        }
+
+        private void openFileDialog2_HelpRequest(object sender, EventArgs e)
+        {
+            MessageBox.Show("Silinecek resimleri seçiniz. Birden fazla resmi seçebilmek" + " için Ctrl tuşuna basılı tutarak istediğiniz resmi seçiniz.");
+        }
+    }
+}
