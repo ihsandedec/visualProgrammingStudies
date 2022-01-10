0a3af60b70282686ad16fd4396058fe59c9bb4cc
ihsandedec <ddcihsan@gmail.com>
2022-01-10 10:57:48 +0300

    Menüleri checkBox gibi kullanma örneği eklendi

---
+++ checkBoxGibiMenuler/sayfa196_checkBoxGibiMenuler/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace sayfa196_checkBoxGibiMenuler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.düzenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kalınToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eğikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altıÇiziliToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.düzenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(273, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // düzenToolStripMenuItem
+            // 
+            this.düzenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kalınToolStripMenuItem,
+            this.eğikToolStripMenuItem,
+            this.altıÇiziliToolStripMenuItem});
+            this.düzenToolStripMenuItem.Name = "düzenToolStripMenuItem";
+            this.düzenToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.düzenToolStripMenuItem.Text = "Düzen";
+            // 
+            // kalınToolStripMenuItem
+            // 
+            this.kalınToolStripMenuItem.Name = "kalınToolStripMenuItem";
+            this.kalınToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kalınToolStripMenuItem.Text = "Kalın";
+            this.kalınToolStripMenuItem.Click += new System.EventHandler(this.kalınToolStripMenuItem_Click);
+            // 
+            // eğikToolStripMenuItem
+            // 
+            this.eğikToolStripMenuItem.Name = "eğikToolStripMenuItem";
+            this.eğikToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.eğikToolStripMenuItem.Text = "Eğik";
+            this.eğikToolStripMenuItem.Click += new System.EventHandler(this.eğikToolStripMenuItem_Click);
+            // 
+            // altıÇiziliToolStripMenuItem
+            // 
+            this.altıÇiziliToolStripMenuItem.Name = "altıÇiziliToolStripMenuItem";
+            this.altıÇiziliToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.altıÇiziliToolStripMenuItem.Text = "Altı Çizili";
+            this.altıÇiziliToolStripMenuItem.Click += new System.EventHandler(this.altıÇiziliToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(12, 164);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 102);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 278);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem düzenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kalınToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eğikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altıÇiziliToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ checkBoxGibiMenuler/sayfa196_checkBoxGibiMenuler/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa196_checkBoxGibiMenuler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void kalınToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (kalınToolStripMenuItem.Checked)
+            {
+                textBox1.Font = new Font(textBox1.Font,textBox1.Font.Style | FontStyle.Bold);
+            }
+            else
+            {
+                textBox1.Font = new Font(textBox1.Font, textBox1.Font.Style ^ FontStyle.Bold);
+            }
+        }
+
+        private void eğikToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (eğikToolStripMenuItem.Checked)
+            {
+                textBox1.Font = new Font(textBox1.Font, textBox1.Font.Style | FontStyle.Italic);
+            }
+            else
+            {
+                textBox1.Font = new Font(textBox1.Font, textBox1.Font.Style ^ FontStyle.Italic);
+            }
+        }
+
+        private void altıÇiziliToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (altıÇiziliToolStripMenuItem.Checked)
+            {
+                textBox1.Font = new Font(textBox1.Font, textBox1.Font.Style | FontStyle.Underline);
+            }
+            else
+            {
+                textBox1.Font = new Font(textBox1.Font, textBox1.Font.Style ^ FontStyle.Underline);
+            }
+        }
+    }
+}
