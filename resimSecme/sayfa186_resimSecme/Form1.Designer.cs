2c6f379003bd9e70a2e70dc34ebbb1cf13057822
ihsandedec <ddcihsan@gmail.com>
2022-01-11 10:22:08 +0300

    Resimleri seçerek işlem yapma örneği eklendi

---
+++ resimSecme/sayfa186_resimSecme/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace sayfa186_resimSecme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_resimyukle = new System.Windows.Forms.Button();
+            this.button_kes = new System.Windows.Forms.Button();
+            this.button_sagacevir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_resimyukle
+            // 
+            this.button_resimyukle.Location = new System.Drawing.Point(60, 44);
+            this.button_resimyukle.Name = "button_resimyukle";
+            this.button_resimyukle.Size = new System.Drawing.Size(98, 36);
+            this.button_resimyukle.TabIndex = 0;
+            this.button_resimyukle.Text = "Resmi Yükle";
+            this.button_resimyukle.UseVisualStyleBackColor = true;
+            this.button_resimyukle.Click += new System.EventHandler(this.button_resimyukle_Click);
+            // 
+            // button_kes
+            // 
+            this.button_kes.Location = new System.Drawing.Point(208, 44);
+            this.button_kes.Name = "button_kes";
+            this.button_kes.Size = new System.Drawing.Size(102, 36);
+            this.button_kes.TabIndex = 1;
+            this.button_kes.Text = "Kes";
+            this.button_kes.UseVisualStyleBackColor = true;
+            this.button_kes.Click += new System.EventHandler(this.button_kes_Click);
+            // 
+            // button_sagacevir
+            // 
+            this.button_sagacevir.Location = new System.Drawing.Point(364, 44);
+            this.button_sagacevir.Name = "button_sagacevir";
+            this.button_sagacevir.Size = new System.Drawing.Size(109, 36);
+            this.button_sagacevir.TabIndex = 2;
+            this.button_sagacevir.Text = "20 Derece Sağa Çevir";
+            this.button_sagacevir.UseVisualStyleBackColor = true;
+            this.button_sagacevir.Click += new System.EventHandler(this.button_sagacevir_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(60, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button_sagacevir);
+            this.Controls.Add(this.button_kes);
+            this.Controls.Add(this.button_resimyukle);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_resimyukle;
+        private System.Windows.Forms.Button button_kes;
+        private System.Windows.Forms.Button button_sagacevir;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ resimSecme/sayfa186_resimSecme/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa186_resimSecme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Rectangle secili_alan = new Rectangle(0, 0, 0, 0);
+
+        private void button_resimyukle_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Resim dosyaları | .bmp;.jpeg;.gif;.tif";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                secili_alan.X = e.X;
+                secili_alan.Y = e.Y;
+                secili_alan.Width = 0;
+                secili_alan.Height = 0;
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                secili_alan.Width = Math.Abs(secili_alan.Left - e.X);
+                secili_alan.Height = Math.Abs(secili_alan.Top - e.Y);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(new Pen(Color.Red), secili_alan);
+        }
+
+        private void button_kes_Click(object sender, EventArgs e)
+        {
+            if ((secili_alan.Height == 0) || (secili_alan.Width == 0))
+            {
+                MessageBox.Show("Seçili alan yok");
+                return;
+            }
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Resim yok");
+                return;
+            }
+            Bitmap secili_resim = new Bitmap(secili_alan.Width, secili_alan.Height);
+            Graphics grafik = Graphics.FromImage(secili_resim);
+            grafik.DrawImage(pictureBox1.Image, 0, 0, secili_alan, GraphicsUnit.Pixel);
+            pictureBox1.Image = secili_resim;
+            grafik.Dispose();
+
+            secili_alan.X = 0;
+            secili_alan.Y = 0;
+            secili_alan.Width = 0;
+            secili_alan.Height = 0;
+        }
+
+        private void button_sagacevir_Click(object sender, EventArgs e)
+        {
+            if ((secili_alan.Height == 0) || (secili_alan.Width == 0))
+            {
+                MessageBox.Show("Seçili alan yok");
+                return;
+            }
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Resim yok");
+                return;
+            }
+            Bitmap secili_resim = new Bitmap(secili_alan.Width, secili_alan.Height);
+            Graphics grafik = Graphics.FromImage(secili_resim);
+            grafik.RotateTransform(20);
+            grafik.TranslateTransform(0, 0, System.Drawing.Drawing2D.MatrixOrder.Append);
+            grafik.DrawImage(pictureBox1.Image, 0, 0, secili_alan, GraphicsUnit.Pixel);
+            Graphics g;
+            g = pictureBox1.CreateGraphics();
+            g.DrawImage(secili_resim, secili_alan);
+            grafik.Dispose();
+            g.Dispose();
+        }
+    }
+}
