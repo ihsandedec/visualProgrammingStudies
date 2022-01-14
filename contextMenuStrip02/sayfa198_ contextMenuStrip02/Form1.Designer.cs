9cb0aebfc5e6a4c4289a2e008205870eab2ec3c1
ihsandedec <ddcihsan@gmail.com>
2022-01-14 15:04:47 +0300

    ContextMenuStript ikinci örneği eklendi

---
+++ contextMenuStrip02/sayfa198_ contextMenuStrip02/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace sayfa198__contextMenuStrip02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.renkDeğiştirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.simgeDurumunuKüçültToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ekranıKapaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.renkDeğiştirToolStripMenuItem,
+            this.simgeDurumunuKüçültToolStripMenuItem,
+            this.ekranıKapaToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(206, 70);
+            // 
+            // renkDeğiştirToolStripMenuItem
+            // 
+            this.renkDeğiştirToolStripMenuItem.Name = "renkDeğiştirToolStripMenuItem";
+            this.renkDeğiştirToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.renkDeğiştirToolStripMenuItem.Text = "Renk Değiştir";
+            this.renkDeğiştirToolStripMenuItem.Click += new System.EventHandler(this.renkDeğiştirToolStripMenuItem_Click);
+            // 
+            // simgeDurumunuKüçültToolStripMenuItem
+            // 
+            this.simgeDurumunuKüçültToolStripMenuItem.Name = "simgeDurumunuKüçültToolStripMenuItem";
+            this.simgeDurumunuKüçültToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.simgeDurumunuKüçültToolStripMenuItem.Text = "Simge Durumunu Küçült";
+            this.simgeDurumunuKüçültToolStripMenuItem.Click += new System.EventHandler(this.simgeDurumunuKüçültToolStripMenuItem_Click);
+            // 
+            // ekranıKapaToolStripMenuItem
+            // 
+            this.ekranıKapaToolStripMenuItem.Name = "ekranıKapaToolStripMenuItem";
+            this.ekranıKapaToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.ekranıKapaToolStripMenuItem.Text = "Ekranı Kapla";
+            this.ekranıKapaToolStripMenuItem.Click += new System.EventHandler(this.ekranıKapaToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 278);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem renkDeğiştirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem simgeDurumunuKüçültToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ekranıKapaToolStripMenuItem;
+    }
+}
+
---
+++ contextMenuStrip02/sayfa198_ contextMenuStrip02/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayfa198__contextMenuStrip02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void renkDeğiştirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            this.BackColor = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+        }
+
+        private void simgeDurumunuKüçültToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ContextMenuStrip = contextMenuStrip1;
+        }
+
+        private void ekranıKapaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
